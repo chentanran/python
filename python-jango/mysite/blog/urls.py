195d74292049579ed05814857d1b8ce2ab5a1cd2
tanran <215920697@qq.com>
2020-01-14 23:46:06 +0800

    博客搭建完成

---
+++ python-jango/mysite/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import  views
+
+urlpatterns = [
+    # path()第一个参数为空，表示访问根，又因为在blog应用中，所以访问blog应用的根，即http://127.0.0.1:8000/blog/
+    # views.blog_title声明了响应这个请求的函数
+    path('', views.blog_title),
+    path('<int:article_id>/', views.blog_article)
+]
--- python-jango/mysite/blog/views.py
+++ python-jango/mysite/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import BlogArticles
 # Create your views here.
+
+def blog_title(request):
+    blogs = BlogArticles.objects.all()
+    return render(request, 'blog/titles.html', {'blogs': blogs})
+
+# 查看文章请求详情
+def blog_article(request, article_id):
+    article = BlogArticles.objects.get(id=article_id)
+    pub = article.publish
+    return render(request, 'blog/content.html', { 'article': article, 'publish': pub })
